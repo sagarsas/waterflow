10da6a0a285a99387b26f6a533bfc5c0a37ef20b
Sajid Rahman <sajidur1993@gmail.com>
2018-02-13 22:59:39 +0530

    added dropdown

--- js/product.js
+++ js/product.js
@@ -13,4 +13,5 @@ $( document ).ready(function() {
                   $(".button-collapse").sideNav({
                       edge:"right"
                   });
+                    $(".dropdown-button").dropdown();
                 });
