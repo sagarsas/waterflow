21c81a5d89ab184fe65a4c24db26670d2b7b7861
Sajid Rahman <sajidur1993@gmail.com>
2018-02-13 21:37:54 +0530

    added side nav and header

--- js/product.js
+++ js/product.js
@@ -1,10 +1,16 @@
-$(document).ready(function() {
-  $('.collapse.in').prev('.panel-heading').addClass('active');
-  $('#accordion, #bs-collapse')
-    .on('show.bs.collapse', function(a) {
-      $(a.target).prev('.panel-heading').addClass('active');
-    })
-    .on('hide.bs.collapse', function(a) {
-      $(a.target).prev('.panel-heading').removeClass('active');
-    });
-});
+//$(document).ready(function() {
+//  $('.collapse.in').prev('.panel-heading').addClass('active');
+//  $('#accordion, #bs-collapse')
+//    .on('show.bs.collapse', function(a) {
+//      $(a.target).prev('.panel-heading').addClass('active');
+//    })
+//    .on('hide.bs.collapse', function(a) {
+//      $(a.target).prev('.panel-heading').removeClass('active');
+//    });
+//});
+
+$( document ).ready(function() {
+                  $(".button-collapse").sideNav({
+                      edge:"right"
+                  });
+                });
